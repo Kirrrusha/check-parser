e264df26886a32412986f3a17367727a4cc2fc4f
kirrrrusha <kirilllebedenko@gmail.com>
2023-10-08 15:58:13 +0300

    come back axios

--- parser.js
+++ parser.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 const cheerio = require('cheerio');
-const https = require('https');
+const axios = require('axios');
 const { sendTelegramMessage } = require('./telegram.js')
 
-const telegramAdminId = process.env.TELEGRAM_ADMIN_ID;
-const telegramIds = (process.env.TELEGRAM_ID_LIST || '').split(',').concat(telegramAdminId);
+  const telegramAdminId = process.env.TELEGRAM_ADMIN_ID;
+  const telegramIds = (process.env.TELEGRAM_ID_LIST || '').split(',').concat(telegramAdminId);
 
-async function ParserTickets() {
-  const hostname = process.env.HOST;
+ async function ParserTickets() {
   const sitesUrl = (process.env.SITES_URL || '').split(',');
 
   const titleSelector = 'h1';
@@ -19,51 +18,24 @@ async function ParserTickets() {
   try {
     for (const url of sitesUrl) {
       try {
-
-        const options = {
-          hostname,
-          port: 443,
-          path: url,
-          method: 'GET',
-        };
-
-        const req = https.request(options, (res) => {
-          let data = '';
-
-          res.on('data', (chunk) => {
-            data += chunk;
-          });
-
-          res.on('end', async () => {
-            await Check(data)
-          });
-        });
-
-        req.on('error', (error) => {
-          console.error('Ошибка:', error);
-          sendTelegramMessage(telegramAdminId, `Ошибка: ${String(error)}`)
-        });
-
-        req.end();
-
-        // const response = await axios.get(url, {timeout: 5000});
-        // const html = response.data;
-        async function Check(response) {
-          const $ = cheerio.load(response);
+        const response = await axios.get(url);
+        const html = response.data;
+        const $ = cheerio.load(html);
 
         const parentElement = $(sectorListSelector);
         const emptyElement = $(emptySelector);
         const titleText =  $(titleSelector).text();
         const dateText = $(dateSelector).text();
 
-        if(!!emptyElement.length) {
+        if(emptyElement.length) {
           console.log(`${new Date().toISOString()} Нет билетов на ${String(dateText.trim())}`);
           sendTelegramMessage(telegramAdminId, `${titleText}: нет билетов на ${String(dateText.trim())}`)
-          return;
+          continue;
         }
 
-        if (!!parentElement.length) {
-          const sectors = $(`${sectorSelector}`);
+        if (parentElement.length) {
+          const sectors = $(`${sectorListSelector} ${sectorSelector}`);
+
           sectors.each((_, element) => {
             const text =  $(element).find('span').text();
             console.log(`${new Date().toISOString()} ${titleText}: билет ${text}` );
@@ -71,14 +43,11 @@ async function ParserTickets() {
               sendTelegramMessage(telegramId, `${titleText}: билет ${text}`)
             })
           })
-
         } else {
           console.error('Родительский элемент не найден');
         }
-      }
-
       } catch (error) {
-        console.error('Ошибка при загрузке HTML:', error);
+        console.error('Ошибка при загрузке HTML:', String(error));
         sendTelegramMessage(telegramAdminId, `Ошибка при загрузке HTML: ${String(error)}`)
       }
     }
