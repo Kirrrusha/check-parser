d2a171d18f67ea283231bde8ac1b69275e986417
kirrrrusha <kirilllebedenko@gmail.com>
2023-10-08 13:58:24 +0300

    migrate from axios to https

--- parser.js
+++ parser.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const cheerio = require('cheerio');
-const axios = require('axios');
+const https = require('https');
 const { sendTelegramMessage } = require('./telegram.js')
 
-  const telegramAdminId = process.env.TELEGRAM_ADMIN_ID;
-  const telegramIds = (process.env.TELEGRAM_ID_LIST || '').split(',').concat(telegramAdminId);
+const telegramAdminId = process.env.TELEGRAM_ADMIN_ID;
+const telegramIds = (process.env.TELEGRAM_ID_LIST || '').split(',').concat(telegramAdminId);
 
- async function ParserTickets() {
+async function ParserTickets() {
+  const hostname = process.env.HOST;
   const sitesUrl = (process.env.SITES_URL || '').split(',');
 
   const titleSelector = 'h1';
@@ -18,84 +19,68 @@ const { sendTelegramMessage } = require('./telegram.js')
   try {
     for (const url of sitesUrl) {
       try {
-        const response = await axios.get(url);
-        const html = response.data;
-        const $ = cheerio.load(html);
+
+        const options = {
+          hostname,
+          port: 443,
+          path: url,
+          method: 'GET',
+        };
+
+        const req = https.request(options, (res) => {
+          let data = '';
+
+          res.on('data', (chunk) => {
+            data += chunk;
+          });
+
+          res.on('end', async () => {
+            // const $ = cheerio.load(data);
+            await Check(data)
+          });
+        });
+
+        req.on('error', (error) => {
+          console.error('Ошибка:', error);
+        });
+
+        req.end();
+
+        // const response = await axios.get(url, {timeout: 5000});
+        // const html = response.data;
+        async function Check(response) {
+          const $ = cheerio.load(response);
 
         const parentElement = $(sectorListSelector);
         const emptyElement = $(emptySelector);
         const titleText =  $(titleSelector).text();
         const dateText = $(dateSelector).text();
 
-        if(!!emptyElement) {
+        if(!!emptyElement.length) {
           console.log(`${new Date().toISOString()} Нет билетов на ${String(dateText.trim())}`);
-          telegramIds.forEach(telegramId => {
-            sendTelegramMessage(telegramId, `${titleText}: нет билетов на ${String(dateText.trim())}`)
-          })
-          continue;
+          sendTelegramMessage(telegramAdminId, `${titleText}: нет билетов на ${String(dateText.trim())}`)
+          return;
         }
 
-        if (parentElement) {
-          const sectors = $(`${sectorListSelector} ${sectorSelector}`);
-
-          for (const sector of sectors) {
-            const text = await sector.text();
-            console.log(`${new Date().toISOString()} Текст sector: ${text}` );
+        if (!!parentElement.length) {
+          const sectors = $(`${sectorSelector}`);
+          sectors.each((_, element) => {
+            const text =  $(element).find('span').text();
+            console.log(`${new Date().toISOString()} ${titleText}: билет ${text}` );
             telegramIds.forEach(telegramId => {
               sendTelegramMessage(telegramId, `${titleText}: билет ${text}`)
             })
-          }
+          })
+
         } else {
           console.error('Родительский элемент не найден');
         }
-      } catch (error) {
-        console.error('Ошибка при загрузке HTML:', error);
       }
 
-
-
-        // const parentElement = $(sectorListSelector);
-
-
-        // const pageTitle = $('title').text();
-        // console.log(`Заголовок страницы: ${pageTitle}`);
-
-      // const browser = await puppeteer.launch({ headless: 'new' });
-
-      // const page = await browser.newPage();
-
-      // await page.goto(url);
-      // const parentElement = await page.$(sectorListSelector);
-      // const emptyElement = await page.$(emptySelector);
-      // const dateElement = await page.$(dateSelector);
-      // const titleElement = await page.$(titleSelector);
-      // const titleText = await page.evaluate(el => el.textContent, titleElement).then((text) => text.trim()).catch(error => console.error('textContent', String(error)));
-      // const dateText = await page.evaluate(el => el.textContent, dateElement).then((text) => text.trim()).catch(error => console.error('dateText', String(error)));
-
-      // if(!!emptyElement) {
-      //   await browser.close();
-      //   console.log(`${new Date().toISOString()} Нет билетов на ${String(dateText.trim())}`);
-      //   telegramIds.forEach(telegramId => {
-      //     sendTelegramMessage(telegramId, `${titleText}: нет билетов на ${String(dateText.trim())}`)
-      //   })
-      //   continue;
-      // }
-
-      // if (parentElement) {
-      //   const sectors = await parentElement.$$(sectorSelector);
-
-      //   for (const sector of sectors) {
-      //     const text = await sector.evaluate(node => node.textContent);
-      //     console.log(`${new Date().toISOString()} Текст sector: ${text}` );
-      //     telegramIds.forEach(telegramId => {
-      //       sendTelegramMessage(telegramId, `${titleText}: билет ${text}`)
-      //     })
-      //   }
-      // } else {
-      //   console.log('Родительский элемент не найден');
-      // }
-
-      // await browser.close();
+      } catch (error) {
+        console.error('Ошибка при загрузке HTML:', error);
+        sendTelegramMessage(telegramAdminId, `Ошибка при загрузке HTML: ${String(error)}`)
+      }
     }
   } catch (error) {
     console.error(`${new Date().toISOString()} Произошла ошибка ${String(error)}`);
